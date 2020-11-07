7d52e1858757e4abe86be0d81afd77859ae3e944
Neto Rossetti <netinho.rossetti@gmail.com>
2020-11-07 12:54:01 -0300

    conclusão do desafio 4

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,23 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = <Balance>{ income: 0, outcome: 0, total: 0 }
+    this.transactions.map(t => {
+      balance.income += t.type ===  'income' ? t.value : 0
+      balance.outcome += t.type ===  'outcome' ? t.value : 0
+    })
+    balance.total = balance.income - balance.outcome
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,15 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance()
+      if (value > total) {
+        throw Error('Voce não posusi saldo para esta transação')
+      }
+    }
+    const transaction = this.transactionsRepository.create({ title, value, type })
+    return transaction
   }
 }
 
